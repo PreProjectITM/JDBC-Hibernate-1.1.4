35a1c1403c230bdab8f07df6c48f970b80e00e44
NordTort <41715818+NorthTort@users.noreply.github.com>
2023-07-05 06:16:39 +0300

    New branch0.1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -20,7 +20,6 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
     @Override
     public void createUsersTable() {
         try(Session session = Util.getSessionFactory().openSession()) {
